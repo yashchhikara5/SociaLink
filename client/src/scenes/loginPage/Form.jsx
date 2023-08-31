7efa759c577c3ebb1b9fbe9c5fa83deb5527908b
yashchhikara5 <yashchhikara5@gmail.com>
2023-08-31 18:24:35 +0530

    added server path

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -66,7 +66,7 @@ const Form = () => {
     formData.append('picturePath', values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://socialink-server.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -83,7 +83,7 @@ const Form = () => {
   const login = async (values, onSubmitProps) => {
 
     const loggedInResponse = await fetch(
-      "http://localhost:3001/auth/login",
+      "https://socialink-server.onrender.com/auth/login",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
